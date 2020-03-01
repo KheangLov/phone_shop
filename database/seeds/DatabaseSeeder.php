d5e16fb12b1f348f3121f11cc3e6e736f3d66c94
kheanglov <lovsokheang@gmail.com>
2020-03-01 11:45:04 +0700

    Change seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -50,14 +50,6 @@ class DatabaseSeeder extends Seeder
 
         DB::table('pages')->insert([
             [
-                'name' => 'Here’s what sets us apart.',
-                'page_type_id' => 1,
-                'status' => 1,
-                'position' => 0,
-                'created_at' => $faker->dateTime($max = 'now', $timezone = null),
-                'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
-            ],
-            [
                 'name' => 'The first & only nationwide 5G network.',
                 'page_type_id' => 1,
                 'status' => 1,
@@ -67,15 +59,23 @@ class DatabaseSeeder extends Seeder
             ],
             [
                 'name' => 'NEVER SETTLE',
-                'page_type_id' => 2,
+                'page_type_id' => 1,
                 'status' => 1,
                 'position' => 2,
                 'created_at' => $faker->dateTime($max = 'now', $timezone = null),
                 'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
             ],
             [
-                'name' => 'ASUS ROG PHONE',
+                'name' => 'Here’s what sets us apart.',
                 'page_type_id' => 2,
+                'status' => 1,
+                'position' => 0,
+                'created_at' => $faker->dateTime($max = 'now', $timezone = null),
+                'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
+            ],
+            [
+                'name' => 'ASUS ROG PHONE',
+                'page_type_id' => 1,
                 'status' => 1,
                 'position' => 3,
                 'created_at' => $faker->dateTime($max = 'now', $timezone = null),
@@ -118,110 +118,111 @@ class DatabaseSeeder extends Seeder
                 'page_id' => 3,
                 'created_at' => $faker->dateTime($max = 'now', $timezone = null),
                 'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
+            ]
+        ]);
+
+        DB::table('categories')->insert([
+            [
+                'name' => 'Oneplus',
+                'user_id' => 1,
+                'created_at' => $faker->dateTime($max = 'now', $timezone = null),
+                'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
             ],
             [
-                'title' => 'Snapdragon 845 powered ASUS ROG Phone is a real game-changer',
-                'content' => 'The phone has all of the features gamers will want: unprecedented speeds with Gigabit LTE and Multi-Gigabit 60 GHz Wi-Fi, configurable up to a massive 512 GB for plenty of game storage, an advanced cooling system, visual and audio technologies that captivate, and a unique design inspired by the traditional video game console. The Snapdragon 845 supports all of this and more, ensuring that the high-performance ROG Phone lives up to gamers’ unshakable standards.',
-                'page_id' => 4,
-                'thumbnail' => 'images/post/1582734339.png',
+                'name' => 'ASUS',
+                'user_id' => 1,
+                'created_at' => $faker->dateTime($max = 'now', $timezone = null),
+                'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
+            ],
+            [
+                'name' => 'Xiaomi',
+                'user_id' => 1,
+                'created_at' => $faker->dateTime($max = 'now', $timezone = null),
+                'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
+            ],
+            [
+                'name' => 'NUBIA',
+                'user_id' => 1,
                 'created_at' => $faker->dateTime($max = 'now', $timezone = null),
                 'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
             ]
         ]);
 
-        // DB::table('categories')->insert([
-        //     [
-        //         'name' => 'Oneplus',
-        //         'user_id' => 1,
-        //         'created_at' => $faker->dateTime($max = 'now', $timezone = null),
-        //         'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
-        //     ],
-        //     [
-        //         'name' => 'ASUS',
-        //         'user_id' => 1,
-        //         'created_at' => $faker->dateTime($max = 'now', $timezone = null),
-        //         'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
-        //     ],
-        //     [
-        //         'name' => 'Xiaomi',
-        //         'user_id' => 1,
-        //         'created_at' => $faker->dateTime($max = 'now', $timezone = null),
-        //         'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
-        //     ],
-        //     [
-        //         'name' => 'NUBIA',
-        //         'user_id' => 1,
-        //         'created_at' => $faker->dateTime($max = 'now', $timezone = null),
-        //         'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
-        //     ]
-        // ]);
+        DB::table('products')->insert([
+            [
+                'name' => 'OnePlus 7T Pro McLaren',
+                'price' => 749,
+                'discount' => 7,
+                'description' => 'New from oneplus',
+                'thumbnail' => 'images/1582694481.png',
+                'user_id' => 1,
+                'category_id' => 2,
+                'created_at' => $faker->dateTime($max = 'now', $timezone = null),
+                'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
+            ],
+            [
+                'name' => 'Mi Note 10 Pro',
+                'price' => 599,
+                'discount' => 7,
+                'description' => 'New from company',
+                'thumbnail' => 'images/1582698694.png',
+                'user_id' => 1,
+                'category_id' => 4,
+                'created_at' => $faker->dateTime($max = 'now', $timezone = null),
+                'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
+            ],
+            [
+                'name' => 'Asus ROG Phone2 (Republic of Gamers)',
+                'price' => 629,
+                'discount' => 0,
+                'description' => 'Republic of Gamer',
+                'thumbnail' => 'images/1582707642.jpeg',
+                'user_id' => 1,
+                'category_id' => 3,
+                'created_at' => $faker->dateTime($max = 'now', $timezone = null),
+                'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
+            ],
+            [
+                'name' => 'Nubia Red Magic 3S',
+                'price' => 449,
+                'discount' => 0,
+                'description' => 'NUBIA',
+                'thumbnail' => 'images/1582707767.png',
+                'user_id' => 1,
+                'category_id' => 5,
+                'created_at' => $faker->dateTime($max = 'now', $timezone = null),
+                'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
+            ]
+        ]);
 
-        // DB::table('products')->insert([
-        //     [
-        //         'name' => 'OnePlus 7T Pro McLaren',
-        //         'price' => 749,
-        //         'discount' => 7,
-        //         'description' => 'New from oneplus',
-        //         'thumbnail' => 'images/1582694481.png',
-        //         'user_id' => 1,
-        //         'category_id' => 2,
-        //         'created_at' => $faker->dateTime($max = 'now', $timezone = null),
-        //         'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
-        //     ],
-        //     [
-        //         'name' => 'Mi Note 10 Pro',
-        //         'price' => 599,
-        //         'discount' => 7,
-        //         'description' => 'New from company',
-        //         'thumbnail' => 'images/1582698694.png',
-        //         'user_id' => 1,
-        //         'category_id' => 4,
-        //         'created_at' => $faker->dateTime($max = 'now', $timezone = null),
-        //         'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
-        //     ],
-        //     [
-        //         'name' => 'Asus ROG Phone2 (Republic of Gamers)',
-        //         'price' => 629,
-        //         'discount' => 0,
-        //         'description' => 'Republic of Gamer',
-        //         'thumbnail' => 'images/1582707642.jpeg',
-        //         'user_id' => 1,
-        //         'category_id' => 3,
-        //         'created_at' => $faker->dateTime($max = 'now', $timezone = null),
-        //         'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
-        //     ],
-        //     [
-        //         'name' => 'Nubia Red Magic 3S',
-        //         'price' => 449,
-        //         'discount' => 0,
-        //         'description' => 'NUBIA',
-        //         'thumbnail' => 'images/1582707767.png',
-        //         'user_id' => 1,
-        //         'category_id' => 5,
-        //         'created_at' => $faker->dateTime($max = 'now', $timezone = null),
-        //         'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
-        //     ]
-        // ]);
-
-        // DB::table('sliders')->insert([
-        //     [
-        //         'name' => 'image_slider',
-        //         'path' => 'images/slide/1582693552.jpeg',
-        //         'created_at' => $faker->dateTime($max = 'now', $timezone = null),
-        //         'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
-        //     ],
-        //     [
-        //         'name' => 'phonethree',
-        //         'path' => 'images/slide/1582693602.jpeg',
-        //         'created_at' => $faker->dateTime($max = 'now', $timezone = null),
-        //         'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
-        //     ],
-        //     [
-        //         'name' => 'huewei',
-        //         'path' => 'images/slide/1582693669.jpeg',
-        //         'created_at' => $faker->dateTime($max = 'now', $timezone = null),
-        //         'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
-        //     ]
-        // ]);
+        DB::table('sliders')->insert([
+            [
+                'name' => 'image_slider',
+                'path' => 'images/slide/1582693552.jpeg',
+                'created_at' => $faker->dateTime($max = 'now', $timezone = null),
+                'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
+            ],
+            [
+                'name' => 'phonethree',
+                'path' => 'images/slide/1582693602.jpeg',
+                'created_at' => $faker->dateTime($max = 'now', $timezone = null),
+                'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
+            ],
+            [
+                'name' => 'huewei',
+                'path' => 'images/slide/1582693669.jpeg',
+                'created_at' => $faker->dateTime($max = 'now', $timezone = null),
+                'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
+            ]
+        ]);
+            
+        DB::table('posts')->insert(            [
+            'title' => 'Snapdragon 845 powered ASUS ROG Phone is a real game-changer',
+            'content' => 'The phone has all of the features gamers will want: unprecedented speeds with Gigabit LTE and Multi-Gigabit 60 GHz Wi-Fi, configurable up to a massive 512 GB for plenty of game storage, an advanced cooling system, visual and audio technologies that captivate, and a unique design inspired by the traditional video game console. The Snapdragon 845 supports all of this and more, ensuring that the high-performance ROG Phone lives up to gamers’ unshakable standards.',
+            'page_id' => 4,
+            'thumbnail' => 'images/post/1582734339.png',
+            'created_at' => $faker->dateTime($max = 'now', $timezone = null),
+            'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
+        ]);
     }
 }
--- public/js/app.js
+++ public/js/app.js
@@ -63054,8 +63054,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
-  key: "04cde1f5f2dba178839e",
-  cluster: "ap1",
+  key: "",
+  cluster: "mt1",
   encrypted: true
 });
 
@@ -63228,9 +63228,9 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Kheang\laravel\phone_shop\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! D:\Kheang\laravel\phone_shop\resources\sass\app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! D:\Kheang\laravel\phone_shop\resources\sass\admin.scss */"./resources/sass/admin.scss");
+__webpack_require__(/*! D:\kheang\laravel\phone_shop\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! D:\kheang\laravel\phone_shop\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! D:\kheang\laravel\phone_shop\resources\sass\admin.scss */"./resources/sass/admin.scss");
 
 
 /***/ })
